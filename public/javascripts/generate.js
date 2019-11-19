09a839b54ad9b25a0b74f7583d186c669fe39a65
Glitch (stripe-payments-demo-mx-msi) <none>
2019-11-19 22:34:14 +0000

    👀🗾 Updated with Glitch

---
+++ public/javascripts/generate.js
@@ -0,0 +1,19 @@
+/**
+ * @type {Faker.FakerStatic}
+ */
+const faker = window.faker;
+faker.locale = 'es_MX';
+const generateInputTrigger = document.getElementById('generate');
+
+const {name, address, internet} = faker;
+
+generateInputTrigger.addEventListener('click', () => {
+    console.log("generate!")
+
+  document.getElementsByName('name')[0].value = `${name.firstName()} ${name.lastName()}`;
+  document.getElementsByName('email')[0].value = internet.email();
+  document.getElementsByName('address')[0].value = `${address.streetAddress()}`;
+  document.getElementsByName('city')[0].value = `${address.city()}`;
+  document.getElementsByName('state')[0].value = `${address.state()}`;
+  document.getElementsByName('postal_code')[0].value = `${address.zipCode()}`;
+});
